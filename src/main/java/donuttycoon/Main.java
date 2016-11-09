bec861073d5964f226f6022017a9c4da0083a167
Ryan "archaen" Pando <vretha@gmail.com>
2016-11-09 14:40:57 -0700

    Bind Port for Heroku.

--- src/main/java/donuttycoon/Main.java
+++ src/main/java/donuttycoon/Main.java
@@ -4,7 +4,15 @@ import donuttycoon.DonutService;
 
 public class Main {
     public static void main(String[] args) {
+      port(getHerokuAssignedPort());
+      new DonutController(new DonutService());
+    }
 
-        new DonutController(new DonutService());
+    static int getHerokuAssignedPort() {
+      ProcessBuilder processBuilder = new ProcessBuilder();
+      if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"));
+      }
+      return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
